7cefd377ec6769ba8066b193e81fba6ba2d25b89
Matteo Wauters <matteo.wauters@gmail.com>
2022-03-17 17:28:16 +0100

    first version of qaoa optimizer added. not yet compatible with MCTS

--- methods.py
+++ methods.py
@@ -1,11 +1,12 @@
 import math
 from numpy import *
 from scipy import linalg
+from scipy.optimize import minimize
 import numpy as np
 
 import random
 from qutip import * 
-from annealer import Annealer
+from annealer import *
 
 import mcts as mod_mcts
 
@@ -115,3 +116,33 @@ def mcts(data, n_qubit,T, num_frequency_components ,H0,Hf,psi0,psif,ncandidates,
     solution=-0.2+np.array(obslist)%40*0.01
 
     return solution,fidelity
+
+def qaoa(n_qubit, num_frequency_components,num_trot_step, H0, Hf, psi0, psif, ncandidates, cost_function_type, x0=None):
+    
+    annealer = DigitalAnnealer(n_qubit, num_trot_step, H0, Hf, psi0, psif)
+
+    #notation of Zhou et al. PRX 2021 for the Fourier optimization of QAOA
+
+    p_ind=(np.arange(1,num_trot_step+1).reshape([num_trot_step,1])-0.5)*np.pi/num_trot_step
+    q_ind=np.arange(1,num_frequency_components+1).reshape([1,num_frequency_components])-0.5
+    cos_matrix = np.cos(np.dot(p_ind,q_ind))
+    sin_matrix = np.sin(np.dot(p_ind,q_ind))
+
+    def get_reward(x, cost_function_type='energy'):
+        u = x[:num_frequency_components].reshape([num_frequency_components,1])
+        v = x[num_frequency_components:].reshape([num_frequency_components,1])
+        gamma = np.dot(sin_matrix,u).reshape(-1)
+        beta = np.dot(cos_matrix,v).reshape(-1)
+        energy, fidelity = annealer.digital_evo(gamma,beta)
+
+        if cost_function_type == 'energy':
+            return energy[-1]
+        elif cost_function_type == 'fidelity':
+            return 1.0-fidelity[-1]
+        else:
+            raise ValueError(f'wrong cost function type: {cost_function_type}') 
+    if x0 is None:
+        x0 = np.random.rand(2*num_frequency_components)
+    res = minimize(get_reward, x0,args=(cost_function_type),  method='BFGS',options={'gtol':1e-2, 'disp': True})
+ 
+    return res.x, res.fun, res.nfev
