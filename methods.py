b0f3a8d0d89e286f0ba632829edaa95ace4f9dc3
Matteo Wauters <matteo.wauters@gmail.com>
2022-03-18 14:33:41 +0100

    qaoa now works both with frequency and time schedule optimization. the gradient can be passed via a callable function

--- methods.py
+++ methods.py
@@ -117,22 +117,67 @@ def mcts(data, n_qubit,T, num_frequency_components ,H0,Hf,psi0,psif,ncandidates,
 
     return solution,fidelity
 
-def qaoa(n_qubit, num_frequency_components,num_trot_step, H0, Hf, psi0, psif, ncandidates, cost_function_type, x0=None):
-    
-    annealer = DigitalAnnealer(n_qubit, num_trot_step, H0, Hf, psi0, psif)
+def qaoa(n_qubit, num_frequency_components,num_trot_step, H0, Hf, psi0, psif, ncandidates, cost_function_type='energy', x0=None, optimization_space='frequency',
+        jacobian=False):
+    '''
+    Perform QAOA, either optimizing directly the digitized annealing schedule with num_trot_steps components or in the Fourier components
+    Parameters:
+        n_qubit: number of variables in the 3-SAT problem
+        
+        num_frequency_components: number of frequency compnents of the digitized annealing schedule. The imension of the search space is 2*num_frequency_components
+        
+        num_trot_step: number of trotter steps in the digitize annealing. The schedule is defined by 2*num_trot_steps real numbers
+        
+        H0, psi0: initial (mixing) Hamiltonian and initial state
+        
+        Hf, psif: problem Hamiltonian and target ground state
+        
+        ncandidates: number of local minimum search
+        
+        cost_function_type: <energy> or <fidelity>, determines whch cost function is used in thr qaoa
+        
+        x0: starting point for the local minimum search. if None, it is an array of random numbers
+        
+        optimization_space: <frequency> or <t_schedule>, determines whether the local minimum search is performed in the frequency or time domain
+     
+    Returns:
+        solution: list with the ncandidate minima found 
+        reward: list with the ncandidate minimized cost function
+        num_queries: list with the number of function evaluation for each local search   
 
-    #notation of Zhou et al. PRX 2021 for the Fourier optimization of QAOA
+    '''
+    annealer = DigitalAnnealer(n_qubit, num_trot_step,num_frequency_components, H0, Hf, psi0, psif)
+    if cost_function_type == 'fidelity':
+        jacobian=False
 
-    p_ind=(np.arange(1,num_trot_step+1).reshape([num_trot_step,1])-0.5)*np.pi/num_trot_step
-    q_ind=np.arange(1,num_frequency_components+1).reshape([1,num_frequency_components])-0.5
-    cos_matrix = np.cos(np.dot(p_ind,q_ind))
-    sin_matrix = np.sin(np.dot(p_ind,q_ind))
+    if optimization_space =='frequency':
+        #notation of Zhou et al. PRX 2021 for the Fourier optimization of QAOA
+        '''p_ind=(np.arange(1,num_trot_step+1).reshape([num_trot_step,1])-0.5)*np.pi/num_trot_step
+        q_ind=np.arange(1,num_frequency_components+1).reshape([1,num_frequency_components])-0.5
 
-    def get_reward(x, cost_function_type='energy'):
-        u = x[:num_frequency_components].reshape([num_frequency_components,1])
-        v = x[num_frequency_components:].reshape([num_frequency_components,1])
-        gamma = np.dot(sin_matrix,u).reshape(-1)
-        beta = np.dot(cos_matrix,v).reshape(-1)
+        # we create two num_trot_steps x num_frequency_components matrices fro writing the schdule in fourier space
+        cos_matrix = np.cos(np.dot(p_ind,q_ind))
+        sin_matrix = np.sin(np.dot(p_ind,q_ind))'''
+        n_search = num_frequency_components
+
+    elif optimization_space=='t_schedule':
+        n_search = num_trot_step
+    else:
+        raise ValueError(f'wrong optimization space: {optimization_space}')
+
+    def get_reward(x, cost_function_type='energy', optimization_space='frequency'):
+        if optimization_space=='frequency':
+            u = x[:num_frequency_components].reshape([num_frequency_components,1])
+            v = x[num_frequency_components:].reshape([num_frequency_components,1])
+            gamma, beta = annealer.uv2schedule(u,v)
+
+        elif optimization_space=='t_schedule':
+            gamma=x[:num_trot_step]
+            beta=x[num_trot_step:]
+
+        else:
+            raise ValueError(f'wrong optimization space: {optimization_space}')
+
         energy, fidelity = annealer.digital_evo(gamma,beta)
 
         if cost_function_type == 'energy':
@@ -140,9 +185,42 @@ def qaoa(n_qubit, num_frequency_components,num_trot_step, H0, Hf, psi0, psif, nc
         elif cost_function_type == 'fidelity':
             return 1.0-fidelity[-1]
         else:
-            raise ValueError(f'wrong cost function type: {cost_function_type}') 
-    if x0 is None:
-        x0 = np.random.rand(2*num_frequency_components)
-    res = minimize(get_reward, x0,args=(cost_function_type),  method='BFGS',options={'gtol':1e-2, 'disp': True})
- 
-    return res.x, res.fun, res.nfev
+            raise ValueError(f'wrong cost function type: {cost_function_type}')
+    
+    def get_gradient(x, cost_function_type='energy', optimization_space='frequency'):
+
+        if optimization_space=='frequency':
+            u = x[:num_frequency_components].reshape([num_frequency_components,1])
+            v = x[num_frequency_components:].reshape([num_frequency_components,1])
+    
+            gamma, beta = annealer.uv2schedule(u,v)
+            grad_gb = annealer.digital_evo(gamma,beta,get_gradient=True)
+            grad_u = np.dot(grad_gb[:gamma.size],annealer.sin_matrix)
+            grad_v = np.dot(grad_gb[gamma.size:],annealer.cos_matrix)
+            grad_E = np.concatenate((grad_u,grad_v))
+
+        elif optimization_space == 't_schedule':
+            gamma=x[:num_trot_step]
+            beta=x[num_trot_step:]
+
+            grad_E = annealer.digital_evo(gamma,beta,get_gradient=True)
+        
+        return grad_E 
+         
+    solution = []
+    reward = []
+    num_queries = []
+    for n_opt in range(ncandidates):
+        if x0 is None:
+            x0 = np.random.rand(2*n_search)
+        
+        if jacobian: 
+            res = minimize(get_reward, x0, jac=get_gradient, args=(cost_function_type, optimization_space),  method='BFGS',options={'gtol':1e-2, 'disp': True})
+        else: 
+            res = minimize(get_reward, x0,args=(cost_function_type, optimization_space),  method='BFGS',options={'gtol':1e-2, 'disp': True})
+        solution.append(res.x)
+        reward.append(res.fun)
+        num_queries.append(res.nfev)
+        print('ggg', res.jac)
+
+    return solution, reward, num_queries
