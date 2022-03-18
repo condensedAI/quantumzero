7c85d6d263ccda8650d898980a1280a4cf955b74
Matteo Wauters <matteo.wauters@gmail.com>
2022-03-18 12:30:50 +0100

    added calculation of gradients for qaoa

--- annealer.py
+++ annealer.py
@@ -1,5 +1,6 @@
 import numpy as np 
 from qutip import * 
+from qutip.piqs import jspin
 from numpy import *
 
 class Annealer():
@@ -56,7 +57,9 @@ class DigitalAnnealer():
         self.N=2**num_qubits
     
     def x_rotation(self, beta):
-
+        '''
+        Perform a rotation of the angle beta along the x axis. 
+        '''
         single_x_rot = qeye(2)*np.cos(0.5*beta) + 1j*sigmax()*np.sin(0.5*beta)
         op_list = [single_x_rot for nq in range(self.num_qubits)]
 
@@ -64,8 +67,15 @@ class DigitalAnnealer():
 
         return Qobj(op_x_rotation.data.reshape([self.N,self.N]))
 
-    def digital_evo(self, gamma, beta):
+    def digital_evo(self, gamma, beta, get_gradient=False):
+        '''
+        Perform the discrete time evolution of the state psi0 with n_trotter_steps steps. The Hamiltonians used for the evolution are known from the class
+        Parameters:
         
+        gamma: n_trotter_steps dimensional array with the time steps for the evolution with Hf
+        beta: n_trotter_steps dimensional array with the time steps (rotations) for the evolution wit H0
+        get_gradient: if True, computes the derivative of the energy with respect to the variational parameters
+        '''
         assert gamma.size == self.P, f"gamma does not have the correct legth {self.P}" 
         assert beta.size == self.P, f"beta does not have the correct legth {self.P}" 
         
@@ -83,5 +93,21 @@ class DigitalAnnealer():
 
         # Compute the expectation value of the final Hamiltonian at all intermediate steps
         energy=expect(self.Hf, state_m)
-
-        return energy, fidelity
+        
+        if get_gradient:
+            gamma_grad = []
+            beta_grad = []
+            Sx = Qobj(jspin(self.num_qubits,op='x',basis='uncoupled').data.reshape([self.N,self.N]))
+            
+            state_Cm=[self.Hf*state_m[-1]]
+            for m in range(self.P):
+                zphase = 1j*gamma[-1-m]*self.Hf
+                state_Cm.append(zphase.expm(method='sparse')*self.x_rotation(-beta[-1-m])*state_Cm[m])
+      
+            for m in range(self.P):
+                gamma_grad.append(state_Cm[self.P-m].overlap(self.Hf*state_m[m]))  
+                beta_grad.append(state_Cm[self.P-m-1].overlap(self.H0*state_m[m+1]))  
+            return 2*np.concatenate((np.array(gamma_grad).imag, np.array(beta_grad).imag))
+    
+        else:
+            return energy, fidelity
